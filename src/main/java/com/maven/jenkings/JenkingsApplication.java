d4d52761d34a08f15f5824f86ca07f82a7a7f250
Emili Garriga <porsilasmoscasweb@gmail.com>
2022-10-15 11:15:22 +0200

    start

---
+++ src/main/java/com/maven/jenkings/JenkingsApplication.java
@@ -0,0 +1,13 @@
+package com.maven.jenkings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkingsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maven/jenkings/JenkingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maven.jenkings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
